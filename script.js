97c5696c7ae13c98b700f65ad2739dcf8b3a488b
Ayush Sharma <117670536+ayusharma-ctrl@users.noreply.github.com>
2023-02-21 11:07:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+const yourArray = ["apple", 42, true, {one: 1, two: 2}, [ { a: "a", b: "b" }, { c: "c", d: "d" } ]];
 
+var para = document.getElementById('array')
+
+yourArray.map((e)=>{
+	var list = document.createElement('li')
+	list.innerText += e;
+    para.append(list)
+})
